fea14c9f8951e2124169aa3ac168396e280e6f3a
SabinKh <91114252+SabinKh@users.noreply.github.com>
2021-10-14 01:38:26 +0400

    Add files via upload

---
+++ ConsoleApp25/Extension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp25
+{
+    static class Extension
+    {
+        public static void Polendron(this int number)
+        {
+            int sum = 0;
+            for (int i = number; i > 0; i /= 10)
+            {
+                sum = (sum * 10) + i % 10;
+            }
+            if (number == sum)
+            {
+                Console.WriteLine("polendrom ededdir");
+            }
+            else
+            {
+                Console.WriteLine("polendrom eded deyil");
+            }
+        }
+    }
+}
---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 1222;
+            number.Polendron();
+        }
+    }
+}
